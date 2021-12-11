a8dda522fc018055b9b835f21afa1afb6e232a3f
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-10 19:31:26 -0800

    Initializing The Position at 0

---
+++ snakeandladder.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class snakeladder {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Snake Ladder Program");
+        Scanner read = new Scanner(System.in);
+        int num = 0;
+        System.out.print("The position  number is at 0 ");
+
+
+    }
+}
