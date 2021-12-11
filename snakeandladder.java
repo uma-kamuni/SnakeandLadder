32fce15d8779147fdeb82d673d9647fce252c913
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-10 21:54:33 -0800

    Modified: Reports dice roll & its Position after every die

--- snakeandladder.java
+++ snakeandladder.java
@@ -6,41 +6,21 @@ import java.util.Scanner;
 public class snakeladder {
 
     public static void main(String[] args){
-    System.out.println("Welcome to Snake Ladder Program");
-        Scanner read = new Scanner(System.in);
-        int num = 0;
-        int position;
-        System.out.println("The position  number is at 0 ");
-        Random random = new Random();
-        int dice1 = random.nextInt(6)+1;
-        System.out.println("The player rolls dice " +dice1);
-        int noPlay=0 ,ladder=1,snake=2;
-        int dice2 = random.nextInt(3); // the player checks condition for noplay,ladder,snake
-        System.out.println("check Condition : " +dice2);
-        if (dice2 == noPlay){
-            position=dice1;
-            System.out.println("The position remain same at" +position );
-        }else if ( dice2 == ladder){
-            position = num + dice1;
-            System.out.println("The position received ladder so move forward by " +position );
-        }else
-        {
-            position = num - dice1 ;
-            if (position < 0){
-                position=0;}
-            System.out.println("The position received Snake so move backward by " +position);
-        }
+    System.out.println("Welcome to Snake Ladder Program"); 
         //Scanner read = new Scanner(System.in);
-        position = 0;
+        int position = 0;
         System.out.println("The position  number is at 0 "); // position starts from zero
+        int count=0;
         while (position < 100) {
-            dice1 = random.nextInt(6) + 1;
+            count++;
+            Random random = new Random();
+            int dice1 = random.nextInt(6) + 1;
             System.out.println("The player rolls dice " + dice1);
+
             int NOPLAY = 0, LADDER = 1, SNAKE = 2; //Initialising  conditions
-            dice2 = random.nextInt(3); // the player checks condition for noplay,ladder,snake
+            int dice2 = random.nextInt(3); // the player checks condition for noplay,ladder,snake
             System.out.println("check Condition : " + dice2);
 
-        System.out.println("The position number is " +position);
             if (dice2 == NOPLAY) {
                 position = position + dice1;
                 System.out.println("The position remain same at" + position);
@@ -68,5 +48,6 @@ public class snakeladder {
         if (position > 100) {
             position = position;
         }
-    }
+        System.out.println("dice count" + count);
+}
 }
